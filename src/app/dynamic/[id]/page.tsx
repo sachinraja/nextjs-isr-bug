424745a062508108c7889cd0702a95364dde23f5
Sachin Raja <me@sachinraja.com>
2025-07-02 16:39:06 -0700

    add dynamic route

--- src/app/dynamic/[id]/page.tsx
+++ src/app/dynamic/[id]/page.tsx
@@ -1,9 +1,10 @@
 export const revalidate = 5;
 
-export default async function Home() {
+export default async function Home({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
       <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
-        <div>[param]: STATIC</div>
+        <div>[param]: {id}</div>
       <div>[time]: {new Date().toLocaleTimeString()}</div>
       </div>
   );
