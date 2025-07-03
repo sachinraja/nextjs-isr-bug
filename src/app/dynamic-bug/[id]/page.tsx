d7c312231dc83e9768d649ed74b0a96d47fb8902
Sachin Raja <me@sachinraja.com>
2025-07-02 22:18:28 -0700

    separate bug and working route

--- src/app/dynamic-bug/[id]/page.tsx
+++ src/app/dynamic-bug/[id]/page.tsx
@@ -1,9 +1,5 @@
 export const revalidate = 5;
 
-export async function generateStaticParams() {
-  return [{id: '1'}]
-}
-
 export default async function Home({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return (
